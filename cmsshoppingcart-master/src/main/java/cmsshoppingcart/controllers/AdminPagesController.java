d1e4d055645f0b719a98b096b0b703d7f0a36129
Robin paul k <robinpaul288@gmail.com>
2020-05-15 23:47:52 +0530

    Java Project

---
+++ cmsshoppingcart-master/src/main/java/cmsshoppingcart/controllers/AdminPagesController.java
@@ -0,0 +1,147 @@
+package cmsshoppingcart.controllers;
+
+import cmsshoppingcart.models.PageRepository;
+import cmsshoppingcart.models.data.Page;
+import java.util.List;
+import javax.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@RequestMapping("/admin/pages")
+public class AdminPagesController {
+  private final PageRepository pageRepository;
+
+  public AdminPagesController(PageRepository pageRepository) {
+    this.pageRepository = pageRepository;
+  }
+
+  @GetMapping
+  public String index(Model model) {
+    List<Page> pages = pageRepository.findAllByOrderBySortingAsc();
+    model.addAttribute("pages", pages);
+
+    return "admin/pages/index";
+  }
+
+  @GetMapping("/add")
+  public String add(@ModelAttribute Page page) {
+    return "admin/pages/add";
+  }
+
+  @PostMapping("/add")
+  public String add(
+      @Valid Page page, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+    if (bindingResult.hasErrors()) {
+      return "admin/pages/add";
+    }
+
+    redirectAttributes.addFlashAttribute("message", "Page added successfully.");
+    redirectAttributes.addFlashAttribute("alertClass", "alert-success");
+
+    String slug =
+        page.getSlug().equals("")
+            ? page.getTitle().toLowerCase().replace(" ", "-")
+            : page.getSlug().toLowerCase().replace(" ", "-");
+
+    Page slugExists = pageRepository.findBySlug(slug);
+
+    if (slugExists != null) {
+      redirectAttributes.addFlashAttribute(
+          "message", "The slug you selected already exists. Please choose another.");
+      redirectAttributes.addFlashAttribute("alertClass", "alert-danger");
+      redirectAttributes.addFlashAttribute("page", page);
+
+      return "redirect:/admin/pages/add";
+    }
+
+    page.setSlug(slug);
+    page.setSorting(100);
+
+    pageRepository.save(page);
+
+    return "redirect:/admin/pages";
+  }
+
+  @GetMapping("/edit/{id}")
+  public String edit(@PathVariable int id, Model model) {
+    Page page = pageRepository.getOne(id);
+    model.addAttribute("page", page);
+
+    return "admin/pages/edit";
+  }
+
+  @PostMapping("/edit")
+  public String edit(
+      @Valid Page page,
+      BindingResult bindingResult,
+      RedirectAttributes redirectAttributes,
+      Model model) {
+    Page pageCurrent = pageRepository.getOne(page.getId());
+
+    if (bindingResult.hasErrors()) {
+      model.addAttribute("pageTitle", pageCurrent.getTitle());
+
+      return "admin/pages/edit";
+    }
+
+    redirectAttributes.addFlashAttribute("message", "Page edited successfully.");
+    redirectAttributes.addFlashAttribute("alertClass", "alert-success");
+
+    String slug =
+        page.getSlug().equals("")
+            ? page.getTitle().toLowerCase().replace(" ", "-")
+            : page.getSlug().toLowerCase().replace(" ", "-");
+
+    Page slugExists = pageRepository.findBySlugAndIdNot(slug, page.getId());
+
+    if (slugExists != null) {
+      redirectAttributes.addFlashAttribute(
+          "message", "The slug you selected already exists. Please choose another.");
+      redirectAttributes.addFlashAttribute("alertClass", "alert-danger");
+      redirectAttributes.addFlashAttribute("page", page);
+
+      return "redirect:/admin/pages/edit/" + page.getId();
+    }
+
+    page.setSlug(slug);
+
+    pageRepository.save(page);
+
+    return "redirect:/admin/pages";
+  }
+
+  @GetMapping("/delete/{id}")
+  public String delete(@PathVariable int id, RedirectAttributes redirectAttributes) {
+    pageRepository.deleteById(id);
+
+    redirectAttributes.addFlashAttribute("message", "Page deleted successfully.");
+    redirectAttributes.addFlashAttribute("alertClass", "alert-success");
+
+    return "redirect:/admin/pages";
+  }
+
+  @PostMapping("/reorder")
+  public @ResponseBody String reorder(@RequestParam("id[]") int[] id) {
+    int count = 1;
+    Page page;
+
+    for (int pageId : id) {
+      page = pageRepository.getOne(pageId);
+      page.setSorting(count);
+      pageRepository.save(page);
+      count++;
+    }
+
+    return "ok";
+  }
+}
---
+++ cmsshoppingcart-master/src/test/java/cmsshoppingcart/CmsShoppingCartApplicationTests.java
@@ -0,0 +1,13 @@
+package cmsshoppingcart;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CmsShoppingCartApplicationTests {
+  @Test
+  public void contextLoads() {}
+}
